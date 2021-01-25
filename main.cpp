5476160c791bb0756936e63e538e225ee96441e7
M4kulatura <m4kulatura@gmail.com>
2021-01-25 19:12:00 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+string str = "mpg123 ";
+char path[128];
+
+int main() {
+
+    printf("Type in path to your desired audio file: ");
+    cin.getline(path, 128);
+
+    str = str + path + " & mpv --loop $HOME/.config/KarateMp3/visualization/vis.gif"; 
+    const char *command = str.c_str();
+
+    system(command);
+
+    return 0;
+
+}
