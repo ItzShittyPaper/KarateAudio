baeb4ea32093f743292ac462c0e641085f1446d0
M4kulatura <m4kulatura@gmail.com>
2021-02-16 09:10:55 +0100

    new version 1.28

--- main.cpp
+++ main.cpp
@@ -1,21 +1,50 @@
+#include <iostream> 
 #include <cstdlib>
-#include <iostream>
 
 using namespace std;
 
+int option;
 string str = "mpg123 ";
-char path[128];
+string path;
 
 int main() {
-
-    printf("Type in path to your desired audio file: ");
-    cin.getline(path, 128);
-
-    str = str + path + " & mpv --loop $HOME/.config/KarateMp3/visualization/vis.gif";
-    const char *command = str.c_str();
-
-    system(command);
-
-    return 0;
+    
+    printf("choose a option ( 1/2 )");
+    printf("-----------------------");
+    printf("1. Play a file.");
+    printf("2. Show a GIF path.");
+    
+    cin >> option;
+    
+    switch(option) {
+        
+        default: {
             
+            printf("Error 404: there is no such an option");
+            return 404;
+            
+        }
+            
+        case 1: {
+        
+            printf("Type in path to your desired audio file: ");
+            cin >> path;
+        
+            str = str + path + " & mpv --loop $HOME/.config/KarateMp3/visualization/vis.gif";
+            const char *command = str.c_str();
+
+            system(command);
+
+            break;
+        }
+    
+        return 0;
+    
+        case 2: {
+        
+            printf("GIF path: $HOME/.config/KarateMp3/visualization/vis.gif");
+            return 0;
+    
+        }
+    }
 }
