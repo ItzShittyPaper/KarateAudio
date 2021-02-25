3b18995c9888e4d065f53ce2745ba955874f8892
M4kulatura <m4kulatura@gmail.com>
2021-02-25 23:26:24 +0100

    idk what is this shit

--- main.cpp
+++ main.cpp
@@ -1,28 +1,15 @@
+#include <iostream> 
 #include <cstdlib>
-#include <iostream>
 
 using namespace std;
 
-<<<<<<< Updated upstream
-int menu;
-
-=======
 int option;
 int options_option;
->>>>>>> Stashed changes
 string str = "mpg123 ";
-char path[128];
+string path;
 
 int main() {
-
-    std::cout << "What do you want to do?" << endl;
-    printf("1. Play a mp3 / ");
-    printf("2. Show a visualizer path");
-    cin >> menu;
     
-<<<<<<< Updated upstream
-    switch(menu) {
-=======
     printf("choose a option ( 1/2 )");
     printf("-----------------------");
     printf("1. Play a file.");
@@ -31,38 +18,31 @@ int main() {
     cin >> option;
     
     switch(option) {
->>>>>>> Stashed changes
         
         default: {
-        
-            printf("There isn't such an option");
-            exit(0);
-            break;
+            
+            printf("Error 404: there is no such an option");
+            return 404;
             
         }
+            
         case 1: {
         
             printf("Type in path to your desired audio file: ");
-            cin.getline(path, 128);
-
+            cin >> path;
+        
             str = str + path + " & mpv --loop $HOME/.config/KarateMp3/visualization/vis.gif";
             const char *command = str.c_str();
 
             system(command);
 
-            return 0;
             break;
-            
         }
+    
+        return 0;
     
         case 2: {
         
-<<<<<<< Updated upstream
-            printf("Visualizer path is: $HOME/.config/KarateMp3/visualization/vis.gif");
-            exit(0);
-            break;
-        
-=======
     
     		printf("choose a option ( 1/1 for now )");
     		printf("-----------------------");
@@ -87,8 +67,6 @@ int main() {
 			}
 
 		} 
->>>>>>> Stashed changes
         }
-    
     }
 }
