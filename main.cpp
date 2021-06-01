0450af03ae0be7014b908f5618056f7e87e3d458
ItzShittyPaper <m4kulatura@gmail.com>
2021-06-01 20:22:56 +0200

    lol

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ int main(int argc, char **argv) {
     
     cin >> option;
     
-    switch(option) {
+    switch(option)
+	{
         
         default: {
             
@@ -37,37 +38,39 @@ int main(int argc, char **argv) {
             
         case 1: {
 
+			play:
+
         	printf("Type in path to your desired audio file: ");
         	cin >> path;
 
-		fstream config;
-		config.open("config.conf", ios::in);
+			fstream config;
+			config.open("config.conf", ios::in);
 
-		if(config.good() == false) {
+			if(config.good() == false) {
 		
-			cout << "there is no config file";
-			exit(0);
+				cout << "there is no config file";
+				exit(0);
 		
-		}
+			}
 
-		string line;
-		int line_nr = 1;
-		while(getline(config, line)) {
-			switch(line_nr) {
+			string line;
+			int line_nr = 1;
+			while(getline(config, line)) {
+				switch(line_nr) {
 			
-				case 1: visualiser = line; break;
+					case 1: visualiser = line; break;
 			
-			}	
-			line_nr++;
-		}
+				}	
+				line_nr++;
+			}
 
-		config.close();
-		cout << visualiser << endl;
+			config.close();
+			cout << visualiser << endl;
 
-        	if(visualiser == "visualiser = false") {
+        		if(visualiser == "visualiser = false") {
 
                 	int result = 0;
-    			int flags = MIX_INIT_MP3;
+    				int flags = MIX_INIT_MP3;
 
     			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
         			printf("Failed to init SDL\n");
@@ -80,7 +83,7 @@ int main(int argc, char **argv) {
         		exit(1);
     			}
 
-			const char *path_mp3 = path.c_str();
+				const char *path_mp3 = path.c_str();
 
     			Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
     			Mix_Music *music = Mix_LoadMUS(path_mp3);
@@ -92,13 +95,12 @@ int main(int argc, char **argv) {
 
     			Mix_FreeMusic(music);
     			SDL_Quit();
-
-			exit(0);
+				exit(0);
 
         	}
         	else {
 
-                	int result = 0;
+            int result = 0;
     			int flags = MIX_INIT_MP3;
 
     			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
@@ -112,107 +114,9 @@ int main(int argc, char **argv) {
         		exit(1);
     			}
 
-			system("mpv --loop $HOME/.config/KarateMp3/visualisation/vis.gif");
+				system("mpv --loop $HOME/.config/KarateMp3/visualisation/vis.gif");
 
-			const char *path_mp3 = path.c_str();
-
-    			Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-    			Mix_Music *music = Mix_LoadMUS(path_mp3);
-    			Mix_PlayMusic(music, 1);
-
-    			while (!SDL_QuitRequested()) {
-        			SDL_Delay(250);
-    			}
-
-    			Mix_FreeMusic(music);
-    			SDL_Quit();
-
-			return 0;
-
-        	}
-		break;
-        }
-    
-    	case 2: {
-	 
-        	printf("Type in path to your desired audio file: ");
-        	cin >> path;
-
-		fstream config;
-		config.open("config.conf", ios::in);
-
-		if(config.good() == false) {
-		
-			cout << "there is no config file";
-			exit(0);
-		
-		}
-
-		string line;
-		int line_nr = 1;
-		while(getline(config, line)) {
-			switch(line_nr) {
-			
-				case 1: visualiser = line; break;
-			
-			}	
-			line_nr++;
-		}
-
-		config.close();
-		cout << visualiser << endl;
-
-        	if(visualiser == "visualiser = false") {
-
-                	int result = 0;
-    			int flags = MIX_INIT_OGG;
-
-    			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
-        			printf("Failed to init SDL\n");
-        			exit(1);
-    			}
-
-    			if (flags != (result = Mix_Init(flags))) {
-        		printf("Could not initialize mixer (result: %d).\n", result);
-        		printf("Mix_Init: %s\n", Mix_GetError());
-        		exit(1);
-    			}
-
-			const char *path_ogg = path.c_str();
-
-    			Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
-    			Mix_Music *music = Mix_LoadMUS(path_ogg);
-    			Mix_PlayMusic(music, 1);
-
-    			while (!SDL_QuitRequested()) {
-        			SDL_Delay(250);
-    			}
-
-    			Mix_FreeMusic(music);
-    			SDL_Quit();
-
-			exit(0);
-
-        	}
-        	else {
-
-                	int result = 0;
-    			int flags = MIX_INIT_MP3;
-
-    			if (SDL_Init(SDL_INIT_AUDIO) < 0) {
-        			printf("Failed to init SDL\n");
-        			exit(1);
-    			}
-
-    			if (flags != (result = Mix_Init(flags))) {
-        		printf("Could not initialize mixer (result: %d).\n", result);
-        		printf("Mix_Init: %s\n", Mix_GetError());
-        		exit(1);
-    			}
-
-			system("mpv --loop $HOME/.config/KarateMp3/visualisation/vis.gif");
-
-			const char *path_mp3 = path.c_str();
+				const char *path_mp3 = path.c_str();
 
     			Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
     			Mix_Music *music = Mix_LoadMUS(path_mp3);
@@ -227,31 +131,36 @@ int main(int argc, char **argv) {
 
 			return 0;
 			break;
-	}
+        }
+
+    	case 2: {
+			goto play;
+		}		
+
         case 3: {
         
     		cout << "choose a option ( 1/1 for now )" << endl;
     		cout << "-----------------------" << endl;
     		cout << "1. Show a GIF path" << endl;
     		
-		cin >> options_option;
+			cin >> options_option;
 		
-		switch(options_option) {
+			switch(options_option) {
 		
-			default: {
+				default: {
 				 
-            			printf("Error 404: there is no such an option");
-            			return 404;
+            		printf("Error 404: there is no such an option");
+            		return 404;
             
-			}
+				}
 			
-			case 1: {
+				case 1: {
 				
-            			printf("GIF path: $HOME/.config/KarateMp3/visualization/vis.gif");
-            			return 0;		
+            		printf("GIF path: $HOME/.config/KarateMp3/visualization/vis.gif");
+            		return 0;		
 
-			}
-		} 
-        }
-    }
+				} 
+    		}
+		}
+	}
 }
